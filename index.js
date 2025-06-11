ecf679471b705cc43577c4981a86a6464716cb8e
Brian Munyoki <Brianmunyoki168@gmail.com>
2025-06-11 21:49:29 +0300

    training on const

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Brian Muema";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage =`WELCOME TO FLATBOOK, BRIAN MUEMA!`;
+const shortGreeting =`Welcome, Brian!`;
